679e4fa70833b8dfa89dea116241b586f66254f3
Jesus-Adriantf <jesusadriantraviesofernandez@gmail.com>
2024-07-20 09:20:47 -1200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'seguridad_20220066',
+  webDir: 'www'
+};
+
+export default config;
